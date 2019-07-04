15d57c29fac207b9ce26c9dfd6ded719fa9b92bc
Janos <kerezsi1987@gmail.com>
2019-07-04 10:46:08 +0200

    feat(change statuscode in registerSaga)

--- src/sagas/registerSaga.js
+++ src/sagas/registerSaga.js
@@ -7,7 +7,7 @@ export function* registerWorkerSaga() {
     const response = yield call(fetchRegister);
     if (response.data.status === 'error') {
       yield put({ type: 'REGSITER_FAILED', error: response.data.message });
-    } else if (response.data.status === 'HTTP 200') {
+    } else if (response.data.status.statusCode === '200') {
       yield put({ type: 'REGISTER_SUCCESS', payload: response.data });
     }
   } catch (error) {
