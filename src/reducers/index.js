673a6dabeacc82ab9e9df465a296d1699efe1b1d
Janos <kerezsi1987@gmail.com>
2019-07-04 10:38:26 +0200

    feat(add registerReducer to rootReducer)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import loginAuthentication from './loginReducer';
+import registerAuthentication from './registerReducer'
 
 const rootReducer = combineReducers({
   loginAuthentication,
+  registerAuthentication,
   routing: routerReducer,
 });
 
