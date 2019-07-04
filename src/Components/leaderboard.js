cc33d26246040788e82461ca2e6844b111bc4a28
nora <noraselenyi@gmail.com>
2019-07-04 16:57:38 +0200

    feat(HMMM-11): added images

--- src/Components/leaderboard.js
+++ src/Components/leaderboard.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../stylesheets/menubuttons.css';
+import building from '../assets/building.png';
+import troop from '../assets/troop3.png';
 
 
 const leaderboard = () => (
-  <nav>
+  <nav className="menu">
     <button type="submit" >
-      <NavLink to="/kingdom/leaderboard/buildings" className="linkbutton">Buildings</NavLink>
+      <NavLink to="/kingdom/leaderboard/buildings" className="linkbutton">
+        <img src={building} alt="Pic comes here" />
+        <br />
+        Buildings
+      </NavLink>
     </button>
     <button type="submit">
-      <NavLink to="/kingdom/leaderboard/troops" className="linkbutton">Troops</NavLink>
+      <NavLink to="/kingdom/leaderboard/troops" className="linkbutton">
+        <img src={troop} alt="Pic comes here" />
+        <br />
+        Troops
+      </NavLink>
     </button>
   </nav>
 );
--- src/Components/menu/kingdom.js
+++ src/Components/menu/kingdom.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../stylesheets/menubuttons.css';
-import smile from '../../assets/y.png';
+import building from '../../assets/building.png';
+import battle from '../../assets/battle.png';
+import leaderboard from '../../assets/leaderboard.gif';
+import troop from '../../assets/troop3.png';
 
 
 const kingdom = () => (
   <nav className="menu">
     <button type="submit">
       <NavLink to="/kingdom/buildings" className="linkbutton">
-        <img src={smile} alt="Pic comes here" />
+        <img src={building} alt="Pic comes here" />
         <br />
         Buildings
       </NavLink>
     </button>
 
-    <button type="submit">
-      <NavLink to="/kingdom/troops" className="linkbutton">Troops</NavLink>
+    <button type="submit" className="building">
+      <NavLink to="/kingdom/troops" className="linkbutton">
+        <img src={troop} alt="Pic comes here" />
+        <br />
+        Troops
+      </NavLink>
     </button>
 
     <button type="submit">
-      <NavLink to="/kingdom/battle" className="linkbutton">Battle</NavLink>
+      <NavLink to="/kingdom/battle" className="linkbutton">
+        <img src={battle} alt="Pic comes here" />
+        <br />
+        Battle
+      </NavLink>
     </button>
 
     <button type="submit">
-      <NavLink to="/kingdom/leaderboard" className="linkbutton">Leaderboard</NavLink>
+      <NavLink to="/kingdom/leaderboard" className="linkbutton">
+        <img src={leaderboard} alt="Pic comes here" />
+        <br />
+        Leaderboard
+      </NavLink>
     </button>
   </nav>
 );
