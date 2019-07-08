969ee0a05d317dc2acc1546495e10d0e8ad8edb2
nora <noraselenyi@gmail.com>
2019-07-08 17:35:27 +0200

    fix(HMMM-11): Menu fixed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import menu from './menu/menu';
+import Menu from './menu/menu';
 import '../stylesheets/App.css';
 // import leaderboard from './leaderboard';
 import leaderboardContent from '../Containers/leaderboardContent';
@@ -9,13 +9,13 @@ import leaderboardContent from '../Containers/leaderboardContent';
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/kingdom" component={menu} />
-      <Route exact path="/kingdom/buildings" component={menu} />
-      <Route exact path="/kingdom/troops" component={menu} />
-      <Route exact path="/kingdom/battle" component={menu} />
+      <Route exact path="/kingdom" component={Menu} />
+      <Route exact path="/kingdom/buildings" component={Menu} />
+      <Route exact path="/kingdom/troops" component={Menu} />
+      <Route exact path="/kingdom/battle" component={Menu} />
       <Route exact path="/kingdom/leaderboard" component={leaderboardContent} />
-      <Route exact path="/kingdom/leaderboard/buildings" component={menu} />
-      <Route exact path="/kingdom/leaderboard/troops" component={menu} />
+      <Route exact path="/kingdom/leaderboard/buildings" component={Menu} />
+      <Route exact path="/kingdom/leaderboard/troops" component={Menu} />
     </Switch>
   </Router>
 );
