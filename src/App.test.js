d379a318d7637f47a8b7e41a11e4be38fc69a60b
nora <noraselenyi@gmail.com>
2019-07-08 16:20:47 +0200

    fix(HMMM-11): CircleCI fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Kingdom from './Components/menu/kingdom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<HelloWorldSimple />, div);
+  ReactDOM.render(<Kingdom />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
