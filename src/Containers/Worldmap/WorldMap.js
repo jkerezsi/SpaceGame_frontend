6256004739d8d23597ec71526b55fefd10b8825f
Janos <kerezsi1987@gmail.com>
2019-07-15 22:52:35 +0200

    feat(HMMM-28_v2) refactor(worldmap container)

--- src/Containers/Worldmap/WorldMap.js
+++ src/Containers/Worldmap/WorldMap.js
@@ -10,7 +10,8 @@ import {
 } from 'react-simple-maps';
 import mapInfo from './world-50m.json';
 import Button from '../../Components/Button';
-import { listAllCountry } from '../../actions/actions';
+import { listAllCountry, selectCountry } from '../../actions/actions';
+
 
 const wrapperStyles = {
   width: '100%',
@@ -18,28 +19,36 @@ const wrapperStyles = {
   margin: '0 auto',
 };
 
+
 class WorldMap extends Component {
-    state = {
-      selectedCountryId: '',
-      error: '',
-    };
+      state = {
+        selectedCountryId: '',
+        finalcountries: '',
+        error: '',
+      };
 
-    componentWillMount() {
-      const { listAllCountry } = this.props;
-      listAllCountry();
-    }
 
-    handleCountryClick = (countryName) => {
+      async componentWillMount() {
+        const response = await fetch('http://localhost:3012/kingdom/map');
+        const json = await response.json();
+        setTimeout(() => this.loopAll(json), 1000);
+      }
+
+
+  handleCountryClick = (countryName) => {
+    const { finalcountries } = this.state;
+    if (finalcountries.includes(countryName)) {
+      this.setState({ error: 'This country is taken' });
+    } else {
       this.setState({ selectedCountryId: countryName });
-      const { occupiedCountries } = this.props;
-      console.log(occupiedCountries);
-    };
+      this.setState({ error: `You have selected ${countryName}` });
+    }
+  };
 
     submitButton = (e) => {
       e.preventDefault();
       const isValid = this.validate();
       if (isValid === true) {
-        const { selectCountry } = this.props;
         const { selectedCountryId } = this.state;
         selectCountry(selectedCountryId);
         console.log(selectedCountryId);
@@ -50,19 +59,40 @@ class WorldMap extends Component {
     validate = () => {
       const { selectedCountryId } = this.state;
       if (!selectedCountryId) {
-        const { error } = this.state;
         this.setState({ error: 'Please select a country!' });
         return false;
       }
       return true;
     };
 
+    loopAll = (object) => {
+      const all = [];
+      for (let i = 0; i < object.kingdoms.length; i++) {
+        for (let j = 0; j < object.kingdoms[i].location.length; j++) {
+          all.push(object.kingdoms[i].location[j]);
+        }
+      }
+      this.setState({ finalcountries: all });
+      const { finalcountries } = this.state;
+      console.log(finalcountries);
+    }
+
     render() {
-    //   console.log(mapInfo.objects.units.geometries[88].properties.name);
+      console.log(mapInfo.objects);
+      const { error } = this.state;
+      const { finalcountries } = this.state;
+      if (finalcountries === '') {
+        return (
+          <div>
+            <h2 style={{ color: 'white' }}>Loading...</h2>
+          </div>
+        );
+      }
       return (
         <div style={wrapperStyles}>
-          <h1>PLEASE SELECT YOUR COUNTRY</h1>
-          <Button onClick={this.submitButton} buttonText="SELECT" />
+          <h1 style={{ color: 'white' }}>PLEASE SELECT YOUR COUNTRY</h1>
+          <h3 style={{ color: 'white' }}>{ error }</h3>
+          <Button className="selectCountry" onClick={this.submitButton} buttonText="SELECT" />
           <ComposableMap
             projectionConfig={{
               scale: 205,
@@ -77,33 +107,33 @@ class WorldMap extends Component {
           >
             <ZoomableGroup center={[0, 20]} disablePanning>
               <Geographies geography={mapInfo}>
-                {(geographies, projection) => geographies.map((geography, i) => geography.id !== 'ATA' && (
-                <Geography
-                  key={i}
-                  onClick={() => this.handleCountryClick(mapInfo.objects.units.geometries[i].properties.name)}
-                  geography={geography}
-                  projection={projection}
-                  style={{
-                    default: {
-                      fill: '#ECEFF1',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                    hover: {
-                      fill: '#607D8B',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                    pressed: {
-                      fill: '#FF5722',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                  }}
-                />
+                {(geographies, projection) => geographies.map((geography, i) => (
+                  <Geography
+                    key={i}
+                    onClick={() => this.handleCountryClick(mapInfo.objects.units.geometries[i].properties.name)}
+                    geography={geography}
+                    projection={projection}
+                    style={{
+                      default: {
+                        fill: this.state.finalcountries.indexOf(mapInfo.objects.units.geometries[i].properties.name) > -1 ? '#FF0000' : '#ECEFF1',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                      hover: {
+                        fill: '#607D8B',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                      pressed: {
+                        fill: '#FF5722',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                    }}
+                  />
                 ))}
               </Geographies>
             </ZoomableGroup>
