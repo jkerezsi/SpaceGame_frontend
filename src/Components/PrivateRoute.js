12b2fea5e66a1a40febde3a86762f455ffa3e450
Mate Sal <mate.andras.sal@gmail.com>
2019-07-15 10:22:57 +0200

    fix(hmmm-17): localstorage vertification added to authorization and navbar

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,24 +1,16 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
-import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ login, component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={props => (login === 'logged in'
+    render={props => (localStorage.getItem('token')
       ? <Component {...props} />
       : <Redirect to="/login" />
     )}
   />
 );
 
-const mapStateToProps = state => ({
-  login: state.loginAuthentication.status,
-});
-
-export default connect(
-  mapStateToProps,
-  null,
-)(PrivateRoute);
+export default PrivateRoute;
