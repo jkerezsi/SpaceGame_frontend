53abbb29cc9b54f33169dd7832da77f5955908ef
Janos <kerezsi1987@gmail.com>
2019-07-19 15:02:56 +0200

    CI fixes

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {
   Router, Route, Switch,
 } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
 import Navbar from './Navbar';
 import Menu from './menu/menu';
 import Settings from '../Containers/Settings';
 import Login from '../Containers/Login';
 import Register from '../Containers/Register';
 import leaderboardContent from '../Containers/leaderboardContent';
-import addBuildingPlusMenu from '../Containers/addBuilding';
 import { history } from '../store/configureStore';
 import WorldMap from '../Containers/Worldmap/WorldMap';
 import ShowResource from './resources';
