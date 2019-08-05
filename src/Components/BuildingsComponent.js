bde7c59ab008101fc2f2098875e23224b3dc2299
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-08-05 13:26:38 +0200

    fix(HMMM-22-2): eslint solving

--- src/Components/BuildingsComponent.js
+++ src/Components/BuildingsComponent.js
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable arrow-body-stlye */
-/* eslint-disable jsx-a11y/label-has-associated-control */
+eslint-disable react/prop-types
+eslint-disable arrow-body-stlye
+eslint-disable jsx-a11y/label-has-associated-control
 
 
 import React from 'react';
@@ -26,8 +26,8 @@ class GetBuildingsComponent extends React.Component {
         <div className="buildings">
           {building.map((buildings, i) => (
             <div className="building">
-              <h4>
-                Building nr {i + 1}:
+              <h4>Building nr
+                 {i + 1}:
               </h4>
               <h4>
                 {buildings.type}
