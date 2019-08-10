0deb1e7ddaac6a163c531ec9ded4384a5571f763
Blanka Hooz <hooz.blanka@gmail.com>
2019-08-10 13:51:21 +0200

    :fire:

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,7 +8,7 @@ import Login from '../Containers/Login';
 import Register from '../Containers/Register';
 import leaderboardContent from '../Containers/leaderboardContent';
 import { history } from '../store/configureStore';
-import BuildingsContent from '../Containers/BuildingsContent';
+import BuildingsContent from '../Containers/buildingsContent';
 import PrivateRoute from './PrivateRoute';
 import TroopsContent from '../Containers/troopsContent';
 import WorldMap from '../Containers/Worldmap/WorldMap';
